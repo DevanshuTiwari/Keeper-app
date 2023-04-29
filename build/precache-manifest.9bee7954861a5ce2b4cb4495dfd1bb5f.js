76a05a7c2f1d52fc9a61d93c93164f0d36a28b2a
Devanshu Tiwari <77668543+DevanshuTiwari@users.noreply.github.com>
2023-04-29 22:07:36 +0530

    Add files via upload

---
+++ build/precache-manifest.9bee7954861a5ce2b4cb4495dfd1bb5f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "26a5db1f2400a874f2ed",
+    "url": "/static/js/main.26a5db1f.chunk.js"
+  },
+  {
+    "revision": "8d8f48f5789728c58847",
+    "url": "/static/js/1.8d8f48f5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "977d7b35f9ef5b421fd64fa79c985acf",
+    "url": "/index.html"
+  }
+];
